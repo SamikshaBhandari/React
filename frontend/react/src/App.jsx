2faff55816592269cffcf341b88b6d7204381829
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2025-11-10 13:43:31 +0545

    Increase

---
+++ frontend/react/src/App.jsx
@@ -0,0 +1,21 @@
+ import { useState } from "react";
+ const App = () => {
+
+const[count,setCount]=useState(0);
+const increase=()=>{
+  setCount(count+1)
+}
+const decrease=()=>{
+  setCount(count-1)
+}
+
+  return (
+    <>
+    <h1>{count}</h1>
+    <button onClick={increase} >Increase</button>
+    <button onClick={decrease} >Decrease</button>
+    
+    </>
+  )
+}
+export default App;
