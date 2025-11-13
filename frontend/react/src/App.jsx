633892a0e0d6759d50bb182919f1a188e7721759
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2025-11-13 12:55:42 +0545

    Refactor App component to remove unused state and functions

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,20 +1,45 @@
- import { useState } from "react";
- const App = () => {
+//  const App = () => {
 
-const[count,setCount]=useState(0);
-const increase=()=>{
-  setCount(count+1)
-}
-const decrease=()=>{
-  setCount(count-1)
-}
+// const[count,setCount]=useState(0);
+// const increase=()=>{
+//   if(count<10){
+//   setCount(count+1)
+//   }
+// }
+// const decrease=()=>{
+//   if(count>0){
+//   setCount(count-1)
+//   }
+// }
 
-  return (
-    <>
-    <h1>{count}</h1>
-    <button onClick={increase} >Increase</button>
-    <button onClick={decrease} >Decrease</button>
+//   return (
+//     <>
+//     <h1>{count}</h1>
+//     <button onClick={increase} >Increase</button>
+//     <button onClick={decrease} >Decrease</button>
     
+//     </>
+//   )
+// }
+// export default App;
+import {BrowserRouter,Route,Routes}from "react-router-dom"
+import { Home } from "./page/Home";
+import About from "./page/about";
+import Contact from "./page/Contact";
+import Navbar from "./page/Navbar";
+const App=()=>{
+  return(
+    <>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="/" element={<Navbar/>}/>
+
+
+  </Routes>
+</BrowserRouter>
     </>
   )
 }
